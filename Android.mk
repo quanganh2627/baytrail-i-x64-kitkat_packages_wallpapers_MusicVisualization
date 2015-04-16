18795f1ebd9c40f9401e9b155dbfb8e83d558730
Philippe Longepe <philippe.longepe@intel.com>
2015-04-16 15:20:22 +0200

    fix can't build class.dex in the apk file issue.
    Do not preopt to build the class.dex in the apk file.
    
    Change-Id: Ie35e46d2987a030da175d02e0baf3e9f91b55a16
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-30405
    Signed-off-by: Philippe Longepe <philippe.longepe@intel.com>
    Reviewed-on: https://android.intel.com:443/355272

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-u
 LOCAL_PACKAGE_NAME := VisualizationWallpapers
 LOCAL_CERTIFICATE := shared
 LOCAL_SDK_VERSION := 17
-
+LOCAL_DEX_PREOPT := false
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
